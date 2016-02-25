e5b3733ee6243aa1ba586b2405b9a64601a96159
pierreozoux <pierre@ozoux.net>
2016-02-25 12:07:33 +0000

    fix small issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -eux
 
-START = `date +%s`
-
-cd /domains
-
 while true
 do
+  cd /domains
+  START=`date +%s`
   for domain in `ls .`
   do
     echo $domain
     cd /domains/$domain
-    ./scripts/pre-backup
+    if [ -f ./scripts/pre-backup ]
+    then
+      ./scripts/pre-backup
+    fi
     duplicity remove-older-than 10D --force file:///backups/$domain
     duplicity --volsize 500 --full-if-older-than 7D --asynchronous-upload /domains/$domain file:///backups/$domain
   done
-  sleep $((3600 - `date +%s` - $START))
+  sleep $((3600 - `date +%s` + $START))
 done
